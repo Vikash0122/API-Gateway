58dc8cf777c3b76c92487a00289af2f13dfc4037
Vikash Kumar <vikashk.ug21.cs@nitp.ac.in>
2024-02-01 13:59:48 +0530

    Added proxy middleware

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const morgan = require("morgan");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
+
+app.use(morgan("combined"));
+app.use(
+  "/bookingservice",
+  createProxyMiddleware({ target: "http://localhost:3002", changeOrigin: true })
+);
+
+app.get("/home", (req, res) => {
+  return res.json({ message: "OK" });
+});
 
 app.listen(3005, () => {
   console.log("app started at 3005");
