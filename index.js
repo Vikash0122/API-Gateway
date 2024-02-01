77a4a9a7b041787a025174cebff6da1999c877fd
Vikash Kumar <vikashk.ug21.cs@nitp.ac.in>
2024-02-01 14:32:09 +0530

    Added api-rate-limit

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const morgan = require("morgan");
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const rateLimit = require("express-rate-limit");
 
 const app = express();
 
+
+//max 5 request coming from an IP in 2 minutes
+const limiter = rateLimit({
+  windowMs: 2 * 60 * 1000,
+  max: 5,
+});
+
 app.use(morgan("combined"));
+app.use(limiter);
 app.use(
   "/bookingservice",
   createProxyMiddleware({ target: "http://localhost:3002", changeOrigin: true })
